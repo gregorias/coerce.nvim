03f5a6385a5acfa446f1f1ecd76c18a6af5654ee
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-01-04 20:56:59 +0100

    feat(case): implement keyword splitting

--- lua/coerce/case.lua
+++ lua/coerce/case.lua
@@ -2,13 +2,13 @@
 -- @module coerce.case
 local M = {}
 
+local cs = require("coerce.string")
+
 --- Converts a string into a numerical contraction.
 --
 --@param str The string to convert.
 --@return The numerical contraction.
 M.to_numerical_contraction = function(str)
-	local cs = require("coerce.string")
-
 	local grapheme_list = cs.str2graphemelist(str)
 	if #grapheme_list <= 2 then
 		return str
@@ -25,6 +25,72 @@ M.to_numerical_contraction = function(str)
 	end
 
 	return grapheme_list[1] .. character_count .. grapheme_list[#grapheme_list]
+end
+
+--- Splits a word into its parts.
+--
+-- Using ”keyword” instead of “word”, because in this context, things like like
+-- “kebab-case” are not words.
+--
+--@tparam string str The word to split.
+--@treturn table The parts of the word.
+M.split_keyword = function(str)
+	local grapheme_list = cs.str2graphemelist(str)
+	if #grapheme_list <= 2 then
+		return { str }
+	end
+
+	local words = {}
+	local recognized_separators = {
+		["-"] = true,
+		["_"] = true,
+		["."] = true,
+	}
+	local found_separator = nil
+
+	local words = {}
+
+	for i = 1, #grapheme_list, 1 do
+		if recognized_separators[grapheme_list[i]] then
+			found_separator = grapheme_list[i]
+			break
+		end
+	end
+
+	if found_separator then
+		local word = ""
+		for i = 1, #grapheme_list, 1 do
+			if grapheme_list[i] == found_separator then
+				table.insert(words, word)
+				word = ""
+			else
+				word = word .. grapheme_list[i]
+			end
+		end
+		if word ~= "" then
+			table.insert(words, word)
+		end
+	else
+		-- No separator found. Separate by upper case.
+		local word = ""
+		for i = 1, #grapheme_list, 1 do
+			if cs.is_upper(grapheme_list[i]) then
+				if word ~= "" then
+					table.insert(words, word)
+				end
+				word = grapheme_list[i]
+			else
+				word = word .. grapheme_list[i]
+			end
+		end
+		if word ~= "" then
+			table.insert(words, word)
+		end
+	end
+
+	words = vim.tbl_map(vim.fn.tolower, words)
+
+	return words
 end
 
 return M
