5a598c12f5d7346a838bc971f9e4bf306d89bc2c
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-01-17 21:08:21 +0100

    feat(visual): implement get selection

--- lua/coerce/visual.lua
+++ lua/coerce/visual.lua
@@ -24,4 +24,32 @@ M.get_current_visual_mode = function()
 	end
 end
 
+--- Returns the current visual selection.
+--
+-- This function has “current” in its name because a function that returns the
+-- last visual selection is also interesting.
+--
+--@treturn Region The current visual selection.
+M.get_current_visual_selection = function()
+	local cvim = require("coerce.vim")
+	local region = require("coerce.region")
+
+	local vmode = M.get_current_visual_mode()
+	assert(vmode ~= nil, "Not in visual mode.")
+	local rmode = nil
+	if vmode == M.visual_mode.INLINE then
+		rmode = region.modes.CHAR
+	elseif vmode == M.visual_mode.LINE then
+		rmode = region.modes.LINE
+	elseif vmode == M.visual_mode.BLOCK then
+		rmode = region.modes.BLOCK
+	else
+		assert(false, "Invalid visual mode: " .. vmode)
+	end
+
+	local s = cvim.fn.getpos("v")
+	local e = cvim.fn.getpos(".")
+	return region.region(rmode, s, e)
+end
+
 return M
--- tests/coerce/visual_spec.lua
+++ tests/coerce/visual_spec.lua
@@ -1,4 +1,5 @@
 local visual = require("coerce.visual")
+local region = require("coerce.region")
 local test_helpers = require("tests.helpers")
 
 describe("coerce.visual", function()
@@ -14,6 +15,26 @@ describe("coerce.visual", function()
 
 			test_helpers.execute_keys("<esc><C-V>", "x")
 			assert.are.same(visual.visual_mode.BLOCK, visual.get_current_visual_mode())
+		end)
+	end)
+
+	describe("get_current_visual_selection", function()
+		it("gets current inline selection", function()
+			test_helpers.create_buf({ "hello world" })
+			test_helpers.execute_keys("v2l", "x")
+
+			local selection = visual.get_current_visual_selection()
+
+			assert.are.same(region.region(region.modes.CHAR, { 0, 0 }, { 0, 2 }), selection)
+		end)
+
+		it("gets current line selection", function()
+			test_helpers.create_buf({ "hello world" })
+			test_helpers.execute_keys("V", "x")
+
+			local selection = visual.get_current_visual_selection()
+
+			assert.are.same({ mode = region.modes.LINE, start_row = 0, end_row = 1 }, selection)
 		end)
 	end)
 end)
