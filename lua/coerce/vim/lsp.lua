0ffa36202bca0552dca0bf3bcd169740c4eb39a3
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-06-01 10:28:39 +0200

    chore(lsp): replace deprecated get_active_clients

--- lua/coerce/vim/lsp.lua
+++ lua/coerce/vim/lsp.lua
@@ -8,7 +8,7 @@ local M = {}
 -- This function only considers clients connected to the current buffer. We are not interested in
 -- clients that might be somewhere but are meaningless for the purposes of the rename.
 function M.does_any_client_support_rename()
-	local clients = vim.lsp.get_active_clients()
+	local clients = vim.lsp.get_clients()
 	for _, client in pairs(clients) do
 		if vim.lsp.buf_is_attached(0, client.id) and client.supports_method("textDocument/rename") then
 			return true
