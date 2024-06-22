c2f72cbc23657f335e31188d4daaab3504d0e3c6
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-06-22 08:55:41 +0200

    chore: fix doc style

--- lua/coerce/selector.lua
+++ lua/coerce/selector.lua
@@ -2,14 +2,14 @@
 --
 -- A text selector is a function that returns a region of selected text.
 --
---@module coerce.selector
+-- @module coerce.selector
 local M = {}
 
 --- Selects the current word.
 --
 -- This is a fire-and-forget coroutine function.
 --
---@treturn Region The selected region.
+-- @treturn Region The selected region.
 M.select_current_word = function()
 	local operator_m = require("coerce.operator")
 	return operator_m.operator("xn", "iw")
@@ -19,7 +19,7 @@ end
 --
 -- This is a fire-and-forget coroutine function.
 --
---@treturn Region The selected region.
+-- @treturn Region The selected region.
 M.select_with_motion = function()
 	local operator_m = require("coerce.operator")
 	-- The i-mode is important. We might be running within a feedkeys() call, so we need to insert
@@ -33,7 +33,7 @@ end
 -- This plugin is only meant to work with keywords, so this function fails if
 -- the selected region is multiline.
 --
---@treturn Region The selected region or an error.
+-- @treturn Region The selected region or an error.
 M.select_current_visual_selection = function()
 	local visual_m = require("coerce.visual")
 	local selected_region = visual_m.get_current_visual_selection()
