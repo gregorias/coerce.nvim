a803a596824aa268ba8d6a3dbb6c6d259c047493
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-12-13 18:48:56 +0100

    chore: improve annotations

--- lua/coerce/operator.lua
+++ lua/coerce/operator.lua
@@ -21,7 +21,8 @@ M._operator_cb = nil
 
 --- Triggers an operator.
 ---
---- This function sets the operatorfunc option to the provided callback and immediately triggers the operator.
+--- This function sets the operatorfunc option to the provided callback and immediately triggers the
+--- operator.
 --- The operator will use the provided movement in the input buffer.
 ---
 --- The provided callback can be used for dot-repeat.
@@ -30,22 +31,22 @@ M._operator_cb = nil
 M.operator_cb = function(user_operator_cb)
 	M._operator_cb = user_operator_cb
 	vim.o.operatorfunc = "v:lua.require'coerce.operator'._operator_cb"
-	-- 'i' to insert. If we the user has typed "gUe" for uppercasing till end of the word and `gU` calls `operator_cb`,
-	-- then `g@` needs to be inserted before the 'e'.
+	-- 'i' to insert. If we the user has typed "gUe" for uppercasing till end of the word and `gU`
+	-- calls `operator_cb`, then `g@` needs to be inserted before the 'e'.
 	-- 'n' to not remap. We want to trigger the operator.
 	vim.api.nvim_feedkeys("g@", "in", false)
 end
 
 --- Triggers an operator.
---
--- This is a fire-and-forget coroutine function.
---
--- Unlike `operator_cb`, there is no useful callback remaining for the dot-repeat.
--- If you need dot-repeat, use operator_cb.
---
--- @tparam string mode The feedkeys() mode. Using “x” may be important to prevent laziness.
--- @tparam string movement The movement to be used for the operator.
--- @treturn coerce.region.Region The selected region.
+---
+--- This is a fire-and-forget coroutine function.
+---
+--- Unlike `operator_cb`, there is no useful callback remaining for the dot-repeat.
+--- If you need dot-repeat, use operator_cb.
+---
+---@param mode string The feedkeys() mode. Using “x” may be important to prevent laziness.
+---@param movement string The movement to be used for the operator.
+---@return coerce.region.Region selection_region The selected region.
 M.operator = function(mode, movement)
 	local mmode = require("coop.coroutine-utils").cb_to_co(function(cb)
 		M.operator_cb(cb)
