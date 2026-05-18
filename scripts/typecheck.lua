d84cb6d0dcef01440377dc673382ac20bad4b2b5
Grzegorz Milka <grzegorzmilka@gmail.com>
2026-05-18 08:46:15 +0200

    ci: fix typecheck

--- scripts/typecheck.lua
+++ scripts/typecheck.lua
@@ -3,18 +3,7 @@
 
 ---@class Args
 ---@field checklevel string The diagnostics level to fail at
----@field directories string[] The directories to lint
 ---@field configpath string? Path to the luarc.json
-
----@param str string
----@return string[] list_arg
-local function parse_list_args(str)
-	local tbl = {}
-	for arg in str:gmatch("[^\r\n]+") do
-		table.insert(tbl, arg)
-	end
-	return tbl
-end
 
 local function getenv_or_err(env_var)
 	return assert(os.getenv(env_var), env_var .. " not set.")
@@ -22,14 +11,11 @@ end
 
 local workdir = os.getenv("GITHUB_WORKSPACE") or "."
 
-local directory_args = getenv_or_err("INPUT_DIRECTORIES")
 local config_path_input = getenv_or_err("INPUT_CONFIGPATH")
-local directory_list = parse_list_args(directory_args)
 
 ---@type Args
 local args = {
 	checklevel = getenv_or_err("INPUT_CHECKLEVEL"),
-	directories = #directory_list > 0 and directory_list or { "" },
 	configpath = (
 		config_path_input ~= "" and '"' .. workdir .. "/" .. config_path_input .. '"' or nil
 	),
@@ -60,8 +46,8 @@ local function lint(directory)
 	}
 	local stdout_file = "stdout.txt"
 	local stderr_file = "stderr.txt"
-	local cmd = "lua-language-server --check_format=pretty --check "
-		.. directory
+	local cmd = "lua-language-server --check_format=pretty"
+		.. (" --check " .. directory)
 		.. (args.configpath and " --configpath=" .. args.configpath or "")
 		.. " --checklevel="
 		.. args.checklevel
@@ -81,13 +67,11 @@ local function lint(directory)
 end
 
 local success = true
-for _, directory in ipairs(args.directories) do
-	local result = lint('"' .. workdir .. "/" .. directory .. '"')
-	if not result.success then
-		print("Diagnostics for directory " .. result.directory .. ":")
-		print(result.diagnostics)
-		success = false
-	end
+local result = lint('"' .. workdir .. '"')
+if not result.success then
+	print("Diagnostics for directory " .. result.directory .. ":")
+	print(result.diagnostics)
+	success = false
 end
 if not success then
 	error("LINT FAILED!")
--- scripts/typecheck.sh
+++ scripts/typecheck.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-INPUT_CHECKLEVEL=Warning INPUT_DIRECTORIES=$'lua\ntests' INPUT_CONFIGPATH=.luarc.json GITHUB_WORKSPACE="$PWD" lua scripts/typecheck.lua
+INPUT_CHECKLEVEL=Warning INPUT_CONFIGPATH=.luarc.json GITHUB_WORKSPACE="$PWD" lua scripts/typecheck.lua
